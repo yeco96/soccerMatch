14f2e4b536ad9b5020ecfcb14ce039947c8eb8a9
yeco96 <yeisonpa.96@hotmail.com>
2020-04-18 23:53:46 -0600

    Crear Retos a partir de un equipo (publico y privado)

--- src/app/pages/reserva/reserva.page.ts
+++ src/app/pages/reserva/reserva.page.ts
@@ -11,6 +11,8 @@ import {SMS} from '@ionic-native/sms/ngx';
 import {AndroidPermissions} from '@ionic-native/android-permissions/ngx';
 import {MostrarCanchaComponent} from "../../components/mostrar-cancha/mostrar-cancha.component";
 import {Cancha} from "../../models/cancha";
+import {CrearEquipoComponent} from "../../crear-equipo/crear-equipo.component";
+import {Reto, Tipo} from "../../models/reto";
 
 @Component({
     selector: 'app-reserva',
@@ -37,6 +39,8 @@ export class ReservaPage implements OnInit {
     /* Inicializacion de Variables*/
     reservas = new Array<Reserva>();
     filtro: string;
+    usuario: Usuario;
+    tipoReto: boolean;
 
     /*Integracion del crud loader service para la conexion Async y lectura de reservas*/
     ngOnInit() {
@@ -45,9 +49,9 @@ export class ReservaPage implements OnInit {
             const reservas = this.crudService.construir(data) as Array<Reserva>;
 
             this.authService.getDataUser().then(res => {
-                const usuario = res as Usuario;
+                this.usuario = res as Usuario;
                 this.reservas = reservas.filter(reservas => {
-                    return reservas.usuario.id === usuario.id;
+                    return reservas.usuario.id === this.usuario.id;
                 });
                 this.loader.hideLoader();
             }, reason => {
@@ -138,11 +142,71 @@ export class ReservaPage implements OnInit {
         this.mostrarModalAsync(value);
     }
 
+    /*Metodo  para mostrar el modal de crear una cancha*/
+    async crearCancha() {
+        const modal = await this.modalController.create({
+            component: CrearEquipoComponent
+        });
+        await modal.present();
+    }
+
     /*Metodo para agregar un equipo */
-    async agregarEquipo() {
+    async agregarEquipo(reserva) {
+
+
+        await this.authService.getDataUser().then(res => {
+            this.usuario = res as Usuario;
+            this.loader.hideLoader();
+        }, reason => {
+            this.loader.hideLoader();
+        });
+
+        if ((!this.usuario.liderEquipo || this.usuario.liderEquipo === "") && (!this.usuario.perteneceEquipo || this.usuario.perteneceEquipo === "")) {
+            this.presentToast('Error usuario no pertence a un equipo ', false);
+
+
+            const alert = await this.alertController.create({
+                header: '¿Equipo?',
+                message: "Desea crear o unirse a un equipo",
+                buttons: [
+                    {
+                        text: 'Cancelar',
+                        role: 'cancel',
+                        cssClass: 'cancelar',
+                        handler: () => {
+
+                        }
+                    }, {
+                        text: 'Aceptar',
+                        handler: () => {
+                            this.crearCancha();
+                        }
+                    }
+                ]
+            });
+
+            await alert.present();
+
+            return;
+        }
+
+
         const alert = await this.alertController.create({
             header: '¿Agregar un equipo?',
             message: "Al selecionar un equipo se creara un reto",
+            inputs: [{
+                type: "radio",
+                name: 'publico',
+                value: 'publico',
+                label: 'Publico',
+                checked: true
+            }, {
+                type: "radio",
+                name: 'privado',
+                value: 'privado',
+                label: 'Privado',
+                checked: false
+            }],
             buttons: [
                 {
                     text: 'Cancelar',
@@ -153,14 +217,30 @@ export class ReservaPage implements OnInit {
                     }
                 }, {
                     text: 'Aceptar',
-                    handler: () => {
-
+                    handler: data => {
+                        if (!reserva.tipo) {
+                            reserva.tipo = {};
+                        }
+                        reserva.tipo.nombre = data;
+                        this.crearReto(reserva);
                     }
                 }
             ]
         });
 
         await alert.present();
+    }
+
+    /*Metodo  para crear un reto*/
+    crearReto(value: Reto) {
+        this.loader.showLoader();
+        this.crudService.create(this.tables.tablas().RETOS, value).then(resp => {
+            this.presentToast('Se creo el reto', true);
+            this.loader.hideLoader();
+        }).catch(error => {
+            this.presentToast('Ocurrio un error al crear el reto', false);
+            this.loader.hideLoader();
+        });
     }
 
     /*Metodo  para realizar un pago de manera Async*/
